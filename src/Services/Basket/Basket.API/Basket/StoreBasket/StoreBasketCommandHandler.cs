1f40f439bd53c33d73f975ab7b575e26401baf65
egit90 <elie90@gmail.com>
2024-06-05 21:32:24 -0400

    added grpc to client

--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketCommandHandler.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketCommandHandler.cs
@@ -1,18 +1,35 @@
 using Basket.API.Data;
+using Basket.API.Models;
 using BuildingBlocks.CQRS;
+using Discount.Grpcs;
 
 namespace Basket.API.Basket.StoreBasket;
 
-public sealed class StoreBasketCommandHandler(IBasketRepository repository) : ICommandHandler<StoreBasketCommand, string>
+public sealed class StoreBasketCommandHandler(IBasketRepository repository, DiscountProtoService.DiscountProtoServiceClient discountProto) : ICommandHandler<StoreBasketCommand, string>
 {
     public async Task<string> Handle(StoreBasketCommand request, CancellationToken cancellationToken)
     {
-        var cart = request.Cart;
+
+        //  call Discount grpc
+        await DeductDiscount(request.Cart, cancellationToken);
 
         // store Basket in db
-        var res = await repository.StoreBasket(cart, cancellationToken);
+        var res = await repository.StoreBasket(request.Cart, cancellationToken);
 
         // update cache
         return request.Cart.UserName;
     }
+
+
+    private async Task DeductDiscount(ShoppingCart cart, CancellationToken cancellationToken = default)
+    {
+        foreach (var item in cart.Items)
+        {
+            var coupon = await discountProto.GetDiscountAsync(new GetDiscountRequest() { ProductName = item.ProductName }, cancellationToken: cancellationToken);
+            item.Price -= coupon.Amount;
+        }
+
+    }
+
+
 }
