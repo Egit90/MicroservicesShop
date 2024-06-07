6a81386b0df95017f5f5d2d50e57a79235538faf
egit90 <elie90@gmail.com>
2024-06-06 21:34:56 -0400

    grpc client and https settings in dev

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -3,13 +3,14 @@ using Basket.API.Models;
 using BuildingBlocks.Behaviors;
 using BuildingBlocks.Exceptions.Handler;
 using Carter;
+using Discount.Grpcs;
 using HealthChecks.UI.Client;
 using Marten;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add Services
+// Application Services
 builder.Services.AddCarter();
 
 builder.Services.AddMediatR(c =>
@@ -19,7 +20,7 @@ builder.Services.AddMediatR(c =>
     c.AddOpenBehavior(typeof(LoggingBehavior<,>));
 });
 
-
+// Data Services
 
 builder.Services.AddMarten(opts =>
 {
@@ -31,14 +32,18 @@ builder.Services.AddMarten(opts =>
 
 }).UseLightweightSessions();
 
-
-
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
 builder.Services.Decorate<IBasketRepository, CashedBasketRepository>();
 
 builder.Services.AddStackExchangeRedisCache(opts =>
 {
     opts.Configuration = builder.Configuration.GetConnectionString("Redis");
+});
+
+// Grpcs
+builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(opts =>
+{
+    opts.Address = new Uri(builder.Configuration["GrpcSetting:DiscountUrl"]!);
 });
 
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
