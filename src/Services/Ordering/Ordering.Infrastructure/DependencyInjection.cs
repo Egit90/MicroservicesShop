4f712b3c84a82816c968f3a7bce65b91e7fa2054
egit90 <elie90@gmail.com>
2024-06-17 20:07:45 -0400

    added the sql server connection to the DI

--- src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Ordering.Infrastructure.Data;
 
 namespace Ordering.Infrastructure;
 
@@ -7,7 +9,14 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
     {
-        var connectionString = configuration.GetConnectionString("Database");
+        var connectionString = configuration.GetConnectionString("DataBase")
+                                ?? throw new Exception("Ordering api is missing the connection string");
+
+        services.AddDbContext<ApplicationDbContext>(opts =>
+        {
+            opts.UseSqlServer(connectionString);
+        });
+
         return services;
     }
 
