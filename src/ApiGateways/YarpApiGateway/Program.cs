a13293c2dc91cfaa0fc12496e72ddabc97ad08f0
egit90 <elie90@gmail.com>
2024-07-05 18:52:48 -0400

    reverse proxy and rate limiting

--- src/ApiGateways/YarpApiGateway/Program.cs
+++ src/ApiGateways/YarpApiGateway/Program.cs
@@ -1,7 +1,27 @@
+using Microsoft.AspNetCore.RateLimiting;
+
 var builder = WebApplication.CreateBuilder(args);
 // services
+builder.Services.AddReverseProxy()
+        .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+
+builder.Services.AddRateLimiter(r =>
+{
+    r.AddFixedWindowLimiter("fixed", o =>
+    {
+        o.Window = TimeSpan.FromSeconds(10);
+        o.PermitLimit = 5;
+    });
+});
+
+
 var app = builder.Build();
 
 
 //Pipeline
+
+app.UseRateLimiter();
+app.MapReverseProxy();
+
 app.Run();
